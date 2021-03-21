780da7e79bd6171105235e78d69f572508487c9a
Matias Carreño <30509507+DonEste@users.noreply.github.com>
2021-03-21 00:10:37 -0300

    init commit

---
+++ src/main/java/com/test/myhotel/testmyhotel/TestMyhotelApplication.java
@@ -0,0 +1,13 @@
+package com.test.myhotel.testmyhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMyhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMyhotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/myhotel/testmyhotel/TestMyhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.myhotel.testmyhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMyhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
